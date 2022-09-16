b62651fefb758e7231625dc5110254db871bdde5
fgalulu <fionasonny132@gmail.com>
2022-09-16 17:35:39 +0000

    console

--- console.py
+++ console.py
@@ -12,13 +12,13 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb)'
 
-    def quit(self, arg):
+    def do_quit(self, arg):
         """
         Quit command to exit
         """
         return True
 
-    def EOF(self, arg):
+    def do_EOF(self, arg):
         """
         Exits console
         """
