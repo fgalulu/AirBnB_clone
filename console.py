7586135f6d1017d59203a55f677fd5f182951714
fgalulu <fionasonny132@gmail.com>
2022-09-17 07:49:22 +0000

    console task 7

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ console
 """
 
 import cmd
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -28,6 +29,69 @@ class HBNBCommand(cmd.Cmd):
         """
         Overwriting the emptyline method
         """
+    def do_create(self, arg):
+        """
+        Creates a new instance of a class
+        """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            instance = classes[args[0]]()
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance .id)
+        instance.save()
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and is
+        """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in classes:
+            if len(args) > 1:
+                key = args[0] + '.' + args[1]
+                if key in models.storage.all():
+                    models.storage.all().pop(key)
+                    models.storage.save()
+                else:
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesnt exist **")
+
+    def do_all(self, arg):
+        """
+        prints all string rep of all instances based  orn not on the class name
+        """
+        args = shlex.split(arg)
+        obj_list = []
+        if len(args) == 0:
+            for item in models.storage.all().values():
+                obj_lists.appent(str(itme))
+            print("[", end="")
+            pritn(", ".join(obj_list), end="")
+            print("]")
+        elif args[0] in classes:
+            for item in models.storage.all():
+                if arg[0] in item:
+                    obj_list.append(str(models.storage.all()[key]))
+                print("[", end="")
+                print(", ".join(obj_list), end="")
+            else:
+                print("** class doesnt exists **")
+
+    def do_update(self, arg):
+        """
+        Updates an instance based onthe class name and id
+        """
+        args = shlex.split(arg)
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
